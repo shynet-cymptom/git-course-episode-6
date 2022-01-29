65151fa5e92cbb5fa7e9ba6977228eba1788fdb7
shynet <shynet@gmail.com>
2022-01-29 14:34:29 +0200

    training solution

--- src/math-lib.spec.js
+++ src/math-lib.spec.js
@@ -2,6 +2,11 @@ import { expect } from 'chai';
 import * as lib from './math-lib.js';
 
 describe('MathLib', () => {
-  // TODO: Write your own test cases for sum and subtract
-  it('should return correct sum of 22 + 83', () => {});
+  it('should return correct sum of 22 + 83', () => {
+    expect(lib.sum(22, 82)).to.eq(104);
+  });
+
+  it('should return correct subtract of 88 - 12', () => {
+    expect(lib.subtract(88, 12)).to.eq(76);
+  });
 });
