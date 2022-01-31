0dfcf06b0f343c6fec92720a908c8d1a6945e233
shynet <shynet@gmail.com>
2022-01-31 17:49:00 +0200

    add tests and CI

--- src/math-lib.spec.js
+++ src/math-lib.spec.js
@@ -2,6 +2,11 @@ import { expect } from 'chai';
 import * as lib from './math-lib.js';
 
 describe('MathLib', () => {
-  // TODO: Write your own test cases for sum and subtract
-  it('should return correct sum of 22 + 83', () => {});
+  it('should return number 11 of sum 10 + 1', () => {
+    expect(lib.sum(10, 1)).to.eq(11);
+  });
+
+  it('should return number 1 of subtraction of 5 - 4', () => {
+    expect(lib.subtract(5, 4)).to.eq(1);
+  });
 });
